0c7bc550c1b94f93a0400e6c0d046480fcb6b2c5
dianhan8 <dian@fibrcorp.io>
2024-08-11 19:54:13 +0700

    memisahkan navbar ke file baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
-import './app.css';
+import { useState } from "react";
+import "./app.css";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom"
 
-export default function App(){
+export default function App() {
   const [todos, setTodos] = useState([]);
 
   const createTodo = () => {
@@ -10,34 +12,35 @@ export default function App(){
       date: new Date(),
     };
 
-    
-
     setTodos((prev) => [...prev, todo]);
   };
-  return(
+
+  return (
     <div>
-      <div className='header'>
-        <div>
-          <h3 className='header-title poppins-semibold'>TODO LIST</h3>
-          </div>
-      </div>
-      <div className='main-content'>
-        <div className='content-header'>
-          <h3 className='content-header_title'>Activitty</h3> 
+      <Navbar />
+
+      <div className="main-content">
+        <div className="content-header">
+          <h3 className="content-header_title">Activitty</h3>
           <div>
-            <button className='btn btn-primary btn-rounded' onClick={createTodo}>
-            <span className="material-symbols-outlined">add</span>
-              Tambah 
+            <button
+              className="btn btn-primary btn-rounded"
+              onClick={createTodo}
+            >
+              <span className="material-symbols-outlined">add</span>
+              Tambah
             </button>
           </div>
         </div>
 
-     
         <div className="todos">
           {todos.map((item, index) => (
             <div className="todo-item" key={index}>
-              <span>{item.title}</span>
-              <span className='fa fa-trash'></span>
+              <Link to={`/activity/${index}`}>
+                <span>{item.title}</span>
+              </Link>
+
+              <span className="fa fa-trash"></span>
             </div>
           ))}
         </div>
