3b186f692a258631d6893d254bda95202acb936a
dianhan8 <dian@fibrcorp.io>
2024-08-11 19:53:40 +0700

    tambah router di main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './fonts.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Activity from './Activity.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/activity/:activityId",
+    element: <Activity />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
